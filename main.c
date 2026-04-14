2608086ff907bf788f7001bb64c73e3985465563
Frank Rastelli <frankice8910@gmail.com>
2026-04-14 17:14:54 -0400

    Implement shell loop and dynamic input reader

---
+++ main.c
@@ -0,0 +1,71 @@
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MYSH_RL_BUFSIZE 1024
+char *mysh_read_line(void)
+{
+    int bufsize = MYSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
+
+    if (!buffer) {
+        fprintf(stderr, "mysh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // Read a character
+        c = getchar();
+
+        // If we hit EOF, replace it with a null character and return
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
+
+        // If we have exceeded the buffer, reallocate
+        if (position >= bufsize) {
+            bufsize += MYSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "mysh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+void mysh_loop(void) 
+{
+    char *line;
+    char **args;
+    int status;
+
+    do {
+        printf("> ");
+        line = mysh_read_line();
+        args = mysh_split_line(line);
+        status = mysh_execute(args);
+
+        free(line);
+        free(args);
+    } while (status);
+}
+
+int main(int argc, char **argv) 
+{
+    // Load config files
+
+    // Run command loop
+    mysh_loop();
+
+    return EXIT_SUCCESS;
+}
