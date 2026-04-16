53336a44eddcbfaf62dd1d80b26a1d411b059fcc
Frank Rastelli <frankice8910@gmail.com>
2026-04-15 20:27:01 -0400

    Add builtin commands

--- main.c
+++ main.c
@@ -9,6 +9,61 @@
 #define MYSH_TOK_BUFSIZE 64
 #define MYSH_TOK_DELIM " \t\r\n\a"
 
+/*
+Function Declarations for builtin shell commands:
+*/
+int mysh_cd(char **args);
+int mysh_help(char **args);
+int mysh_exit(char **args);
+
+/*
+List of builtin commands, followed by their corresponding functions
+*/
+
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int mysh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+Builtin function implementations
+*/
+int mysh_cd(char **args)
+{
+    if (args[1] == NULL) {
+        fprintf(stderr, "mysh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("mysh");
+        }
+    }
+    return 1;
+}
+
+int mysh_help(char **args)
+{
+    int i;
+    printf("Frank Rastelli's MYSH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for (i = 0; i < mysh_num_builtins(); i++) {
+        printf(" %s\n", builtin_str[i]);
+    }
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int mysh_exit(char **args)
+{
+    return 0;
+}
+
 int mysh_launch(char **args)
 {
     pid_t pid, wpid;
